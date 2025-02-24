f7b6f36e134a5b6744a198b667fb9b7077a31b5f
anikush-yes <anikush@hotmail.com>
2025-02-24 12:28:27 +0200

    modified 01.Test.RegisterUser.cy.js in Tests
    Signed-off-by: anikush-yes <anikush@hotmail.com>

--- cypress/e2e/Tests/01.Test.RegisterUser.cy.js
+++ cypress/e2e/Tests/01.Test.RegisterUser.cy.js
@@ -19,9 +19,9 @@ describe('Automation Exercise Homepage Test', () => {
     cy.contains('New User Signup!').should('be.visible');
 
     // 6. Enter name and email address
-
-    cy.get('[data-qa="signup-name"]').should('be.visible').type('Petras Petrauskas');
-    cy.get('[data-qa="signup-email"]').should('be.visible').type('PP@example.com');
+    
+    cy.get('[data-qa="signup-name"]').should('be.visible').type('Alison Smith');
+    cy.get('[data-qa="signup-email"]').should('be.visible').type('Fakeeeeeee@example.com');
 
     // 7. Click 'Signup' button
 
@@ -34,24 +34,56 @@ describe('Automation Exercise Homepage Test', () => {
 
     // 9. Fill details: Title, Name, Email, Password, Date of birth
 
-    cy.get('input[data-qa="password"]').type('password');
-    cy.get('select[data-qa="days"]').select('1');
-    cy.get('select[data-qa="months"]').select('January');
-    cy.get('select[data-qa="years"]').select('1990');
+    cy.get('input[data-qa="password"]').type('123456');
+    cy.get('select[data-qa="days"]').select('23');
+    cy.get('select[data-qa="months"]').select('May');
+    cy.get('select[data-qa="years"]').select('1999');
 
 
-    // 10. Select checkbox 'Sign up for our newsletter!
+    // 10. 11. Select checkbox 'Sign up for our newsletter! Select checkbox 'Receive special offers from our partners!'
 
     cy.get('#newsletter').click();
     cy.get('#optin').click();
 
-    // 11. Select checkbox 'Receive special offers from our partners!'
+
     // 12. Fill details: First name, Last name, Company, Address, Address2, Country, State, City, Zipcode, Mobile Number
+
+    cy.get('#first_name').type('Alice');
+    cy.get('#last_name').type('Smith');
+    cy.get('#company').type('GlobalTech');
+    cy.get('#address1').type('456 Elm Street');
+    cy.get('#address2').type('Suite 789');
+    cy.get('#country').select('Canada');
+    cy.get('#state').type('Ontario');
+    cy.get('#city').type('Toronto');
+    cy.get('#zipcode').type('M5H 2N2');
+    cy.get('#mobile_number').type('+14165550000');
+
     // 13. Click 'Create Account button'
+
+    cy.get('button[data-qa="create-account"]').click();
+
     // 14. Verify that 'ACCOUNT CREATED!' is visible
+
+    cy.contains('Account Created!').should('be.visible');
+
     // 15. Click 'Continue' button
+
+    cy.contains('Continue').click();
+    
     // 16. Verify that 'Logged in as username' is visible
+
+    cy.contains('Logged in as Alison Smith').should('be.visible');
+
+
     // 17. Click 'Delete Account' button
+
+    cy.contains('Delete Account').click();
+
     // 18. Verify that 'ACCOUNT DELETED!' is visible and click 'Continue' button
+
+    cy.get('[data-qa="account-deleted"]').should('be.visible');
+    cy.get('[data-qa="continue-button"]').click();
+
   });
 });
