693d9a763ab40bf155e966483988c3b61147dd02
anikush-yes <anikush@hotmail.com>
2025-02-24 09:28:13 +0200

    modified 01.Test.RegisterUser.cy.js in Tests
    Signed-off-by: anikush-yes <anikush@hotmail.com>

--- cypress/e2e/Tests/01.Test.RegisterUser.cy.js
+++ cypress/e2e/Tests/01.Test.RegisterUser.cy.js
@@ -2,9 +2,11 @@
 
 describe('Automation Exercise Homepage Test', () => {
 
-  //1-3. Launch browser, navigate to url 'http://automationexercise.com', verify that home page is visible successfully:
 
-  it('Should visit homepage and verify title', () => {
+  it('Should visit homepage, signup, delete acc', () => {
+
+    //1-3. Launch browser, navigate to url 'http://automationexercise.com', verify that home page is visible successfully:
+
     cy.visit('https://automationexercise.com/')
     cy.title().should('include', 'Automation Exercise');
 
@@ -23,15 +25,26 @@ describe('Automation Exercise Homepage Test', () => {
 
     // 7. Click 'Signup' button
 
-   cy.get('button[data-qa="signup-button"]').click(); 
+    cy.get('button[data-qa="signup-button"]').click();
 
 
     // 8. Verify that 'ENTER ACCOUNT INFORMATION' is visible
 
-    cy.get('.title.text-center').contains('Enter Account Information').should('be.visible'); 
+    cy.get('.title.text-center').contains('Enter Account Information').should('be.visible');
 
     // 9. Fill details: Title, Name, Email, Password, Date of birth
-    // 10. Select checkbox 'Sign up for our newsletter!'
+
+    cy.get('input[data-qa="password"]').type('password');
+    cy.get('select[data-qa="days"]').select('1');
+    cy.get('select[data-qa="months"]').select('January');
+    cy.get('select[data-qa="years"]').select('1990');
+
+
+    // 10. Select checkbox 'Sign up for our newsletter!
+
+    cy.get('#newsletter').click();
+    cy.get('#optin').click();
+
     // 11. Select checkbox 'Receive special offers from our partners!'
     // 12. Fill details: First name, Last name, Company, Address, Address2, Country, State, City, Zipcode, Mobile Number
     // 13. Click 'Create Account button'
